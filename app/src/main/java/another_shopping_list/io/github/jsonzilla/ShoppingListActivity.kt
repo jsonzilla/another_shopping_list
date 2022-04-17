1a8f34d7371764a8e8e0ce7edc209bca46a6b1c1
Jeison Cardoso <zkzed6nz2@relay.firefox.com>
2022-04-17 11:49:57 -0300

    Update do new api version

--- app/src/main/java/another_shopping_list/io/github/jsonzilla/ShoppingListActivity.kt
+++ app/src/main/java/another_shopping_list/io/github/jsonzilla/ShoppingListActivity.kt
@@ -2,7 +2,7 @@ package another_shopping_list.io.github.jsonzilla
 
 import android.content.Context
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
